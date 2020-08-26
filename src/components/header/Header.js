ec46a48e5a8a586373eb2d50fd1c74be1a24178b
Alexandro <parashnoktf@gmail.com>
2020-08-26 13:55:40 +0300

    Finish redux and table logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,8 @@
 import {ExcelComponent} from '@core/ExcelComponent'
+import {$} from '@core/dom'
+import {defaultTitle} from '@/constants'
+import {changeTitle} from '@/redux/actions';
+import {debounce} from '@core/utils';
 
 export class Header extends ExcelComponent {
     static className = 'excel__header'
@@ -6,13 +10,20 @@ export class Header extends ExcelComponent {
     constructor($root, options) {
         super($root, {
             name: 'Header',
+            listeners: ['input'],
             ...options
         });
     }
 
+    prepare() {
+        this.onInput = debounce(this.onInput)
+    }
+
     toHTML() {
+        const title = this.store.getState().title || defaultTitle
+
         return `
-            <input type="text" class="input" value="Нова Таблиця">
+            <input type="text" class="input" value="${title}">
             <div>
                 <div class="button">
                     <span class="material-icons">delete</span>
@@ -22,5 +33,11 @@ export class Header extends ExcelComponent {
                 </div>
             </div>
         `
+    }
+
+    onInput(event) {
+        const $target = $(event.target)
+
+        this.$dispatch(changeTitle($target.text()))
     }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -97,17 +97,33 @@ class Dom {
         return this
     }
 
+    attr(name, value) {
+        if (value) {
+            this.$el.setAttribute(name, value)
+            return this
+        }
+
+        return this.$el.getAttribute(name)
+    }
+
     text(text) {
-        if (typeof text === 'string') {
+        if (typeof text !== 'undefined') {
             this.$el.textContent = text
             return this
         }
 
-        if (this.$el.tagName === 'input') {
+        if (this.$el.tagName.toLowerCase() === 'input') {
             return this.$el.value.trim()
         }
 
         return this.$el.textContent.trim()
+    }
+
+    getStyles(styles = []) {
+        return styles.reduce((res, s) => {
+            res[s] = this.$el.style[s]
+            return res
+        }, {})
     }
 }
 
