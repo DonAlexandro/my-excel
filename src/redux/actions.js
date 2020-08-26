98c52b131de227cefb7e5464e2bd1527f3c579f0
Alexandro <parashnoktf@gmail.com>
2020-08-26 20:45:56 +0300

    Finish routing

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,10 @@
 import {
-    CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLES, CHANGE_TITLE
+    CHANGE_TEXT,
+    CHANGE_STYLES,
+    TABLE_RESIZE,
+    APPLY_STYLES,
+    CHANGE_TITLE,
+    UPDATE_DATE
 } from './types'
 
 export function tableResize(data) {
@@ -34,5 +39,11 @@ export function changeTitle(data) {
     return {
         type: CHANGE_TITLE,
         data
+    }
+}
+
+export function updateDate() {
+    return {
+        type: UPDATE_DATE
     }
 }
